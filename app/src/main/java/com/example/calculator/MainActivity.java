eac59953bfa3b4edbf8aaca4949b09062d04b671
marzouki <marzouki.mhammed@gmail.com>
2025-01-11 23:29:51 +0100

    add the second activity

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.calculator;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button btnLaunch = findViewById(R.id.loginButton);
+        btnLaunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Calculator_Contents.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
