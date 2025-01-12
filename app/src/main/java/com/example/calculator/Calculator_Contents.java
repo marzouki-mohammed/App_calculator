2e8a395d4ea61d9a12eb519851f2149e25ddd39a
marzouki <marzouki.mhammed@gmail.com>
2025-01-12 13:34:54 +0100

    add the menu

--- app/src/main/java/com/example/calculator/Calculator_Contents.java
+++ app/src/main/java/com/example/calculator/Calculator_Contents.java
@@ -1,24 +1,59 @@
 package com.example.calculator;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
-public class Calculator_Contents extends AppCompatActivity {
+public class Calculator_Contents extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout layout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calculator_contents);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        layout = findViewById(R.id.drawerlyaut);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.ToolbarMenu);
+
+        // Définir la Toolbar comme barre d'action
+        setSupportActionBar(toolbar);
+
+        // Configurer le ActionBarDrawerToggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                layout,
+                toolbar,
+                R.string.OpenNav,
+                R.string.CloseNav
+        );
+
+        layout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Lier l'écouteur de sélection d'élément de navigation à l'activité
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+
+    // Implémentation de la méthode onNavigationItemSelected
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+
+        }
+        layout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
